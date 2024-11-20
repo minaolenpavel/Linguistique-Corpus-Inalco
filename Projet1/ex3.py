5fbc0acfbba148d5dccdd8df74f0b88509578001
Paul <bellevillepaul03@gmail.com>
2024-11-20 17:54:38 +0100

    ex3 done

---
+++ Projet1/ex3.py
@@ -0,0 +1,8 @@
+import nltk
+text = []
+with open("Projet1\samples\sample2.txt", encoding="UTF-8", mode="r") as file:
+    for line in file:
+        text.append(line)
+
+for line in text:
+    print(nltk.word_tokenize(line))
