9dd63f9c06c2fa8862d5810e3980a2fd643af328
Paul <bellevillepaul03@gmail.com>
2025-05-12 18:21:48 +0200

    premier modele de syllabation sous class, hesitation entre les deux

--- S2/Projet_S2/syllabation.py
+++ S2/Projet_S2/syllabation.py
@@ -1,63 +1,69 @@
 import re
 
-def vowel_or_cons(char:str) -> str:
-    re_vowels = r'[aAeEiIoOuUyY]'
-    re_cons = r'[B-DF-HJ-NP-TV-Zb-df-hj-np-tv-z]'
-    re_end = r'#'
+class Syllabifier:
+    def __init__(self):
+        self.etat_initial:str = 'etat_i'
+        self.etats_finaux:list = ['etat_f']
+        self.etats:list = ['etat_i', 'etat_1', 'etat_2', 'etat_3', 'etat_f']
+        self.transitions:dict = {'etat_i': {'C': 'etat_1', 'V':'etat_2'}, 'etat_1':{'V':'etat_2', 'C':'etat_3'}, 'etat_2':{'C': 'etat_3', 'V': 'etat_2'}, 'etat_3':{'#': 'etat_f', 'V': 'etat_i', 'C': 'etat_i'}}
 
-    if re.match(re_vowels, char):
-        return "V"
-    elif re.match(re_cons, char):
-        return "C"
-    elif re.match(re_end, char):
-        return "#"
+    def vowel_or_cons(self, char:str) -> str:
+        re_vowels = r'[aAeEiIoOuUyY]'
+        re_cons = r'[B-DF-HJ-NP-TV-Zb-df-hj-np-tv-z]'
+        re_end = r'#'
 
-def map_word(word:str) -> str:
-    mapped_word = ""
-    for char in word:
-        mapped_word += vowel_or_cons(char)
-    mapped_word += "#"
-    return mapped_word
+        if re.match(re_vowels, char):
+            return "V"
+        elif re.match(re_cons, char):
+            return "C"
+        elif re.match(re_end, char):
+            return "#"
 
-def combine_map_word(word:str) -> tuple:
-    mapped_word = map_word(word)
-    return (word, mapped_word)
+    def map_word(self, word:str) -> str:
+        mapped_word = ""
+        for char in word:
+            mapped_word += self.vowel_or_cons(char)
+        mapped_word += "#"
+        return mapped_word
 
-etat_initial = 'etat_i'
-etats_finaux = ['etat_f']
-etats = ['etat_i', 'etat_1', 'etat_2', 'etat_3', 'etat_f']
-transitions = {'etat_i': {'C': 'etat_1', 'V':'etat_2'}, 'etat_1':{'V':'etat_2', 'C':'etat_3'}, 'etat_2':{'C': 'etat_3', 'V': 'etat_2'}, 'etat_3':{'#': 'etat_f', 'V': 'etat_i', 'C': 'etat_i'}}
+    def combine_map_word(self, word:str) -> tuple:
+        mapped_word = self.map_word(word)
+        return (word, mapped_word)
 
-msg = "mangerons"
+    def syllabify(self, text:str):
+        msg = text
+        pair = self.combine_map_word(msg)
+        etat_courant = self.etat_initial
+        mot_syll = []
+        syllable = []
+        for i in range(len(pair[1])-1):
+            char = pair[1][i]
+            letter = pair[0][i]
+            if char in self.transitions[etat_courant].keys():
+                print(f"{etat_courant} {char} {letter} {self.transitions[etat_courant][char]}")
+                etat_prev = etat_courant
+                etat_courant = self.transitions[etat_courant][char]
+                if etat_courant in self.etats_finaux:
+                    if i == len(pair[1])-1:
+                        syllable.append(letter)
+                        mot_syll.append(list(syllable)) # Conversion en liste pour faire une copie
+                elif etat_courant in ['etat_1', 'etat_2', 'etat_3']:
+                    syllable.append(letter)
+                elif etat_prev == 'etat_3' and etat_courant == 'etat_i':
+                    mot_syll.append(list(syllable)) # Conversion en liste pour faire une copie
+                    syllable.clear()
+                    syllable.append(letter)
+            else:
+                print("erreur")
+                breakpoint()
+                print(f"{etat_courant} {char} {self.transitions[etat_courant][char]}")
+                break
+        # Repêchage pour les syllabes qui ont pu être épargnées
+        if syllable:
+            mot_syll.append(list(syllable))
 
-pair = combine_map_word(msg)
-etat_courant = etat_initial
-mot_syll = []
-syllable = []
-for i in range(len(pair[1])-1):
-    char = pair[1][i]
-    letter = pair[0][i]
-    if char in transitions[etat_courant].keys():
-        print(f"{etat_courant} {char} {letter} {transitions[etat_courant][char]}")
-        etat_prev = etat_courant
-        etat_courant = transitions[etat_courant][char]
-        if etat_courant in etats_finaux:
-            if i == len(pair[1])-1:
-                syllable.append(letter)
-                mot_syll.append(list(syllable)) # Conversion en liste pour faire une copie
-        elif etat_courant in ['etat_1', 'etat_2', 'etat_3']:
-            syllable.append(letter)
-        elif etat_prev == 'etat_3' and etat_courant == 'etat_i':
-            mot_syll.append(list(syllable)) # Conversion en liste pour faire une copie
-            syllable.clear()
-            syllable.append(letter)
-    else:
-        print("erreur")
-        breakpoint()
-        print(f"{etat_courant} {char} {transitions[etat_courant][char]}")
-        break
+        return mot_syll
 
-if syllable:
-    mot_syll.append(list(syllable))
-
-print(mot_syll)
+if __name__ == "__main__":
+    syllaby = Syllabifier()
+    print(syllaby.syllabify("programmation"))
